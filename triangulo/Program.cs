c4241f447f4d0a17c7a5d5fbe6940441341b30e1
guivolpe <guivolpe@gmail.com>
2019-09-25 11:30:57 -0300

    atualizacao

---
+++ triangulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace triandulo
+{
+    class Program
+    {
+        static void Main(string[] args){
+            
+
+            Console.WriteLine("Tamanho do triangulo");
+            int tamanhoTriangulo = int.Parse (Console.ReadLine());
+            Console.WriteLine("Quantidade de triangulos");
+            int qntTriangulo = int.Parse (Console.ReadLine());
+       
+
+            for (int i = 0; i <qntTriangulo; i ++){
+                string estrelas = "";
+
+                for(int j=0; j <tamanhoTriangulo; j ++){
+                    estrelas += "*";
+                    Console.WriteLine(estrelas);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ vetores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] atividade = new int[6];
+            string numeros = "", tipo = "";
+            for (int i = 1  ;  i < 6  ;  i++)
+            {
+                Console.WriteLine ($"Insira o {i} numero: ");
+                atividade[i] = int.Parse(Console.ReadLine());
+                tipo += atividade[i] % 2 == 1 ? "impar, " : "par, ";
+                numeros += atividade[i] + ", ";
+            }
+            Console.Write($"Seus números são: \n{numeros}\n{tipo}");
+        }
+    }
+}
