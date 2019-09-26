ac3bbfb9313989317d20ca18d67fd530ac7f1c80
guivolpe <guivolpe@gmail.com>
2019-09-26 11:18:25 -0300

    atualizacao

---
+++ temperaturamedia/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace temperaturamedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperaturas = new double[12];
+            double maior = 0;
+            double menor = 0;
+            
+            for(int i = 0; i < 12; i++){
+                Console.Write($"Digite a temperatura do mês {i+1}: "); // i+1 é porque não existe mês 0 - janeiro é o mês 1 (i+1) 
+                temperaturas[i] = double.Parse(Console.ReadLine());
+            }
+
+            // Diz que a maior temperatura e a menor é a primeira informada
+            maior = temperaturas[0];
+            menor = temperaturas[0];
+
+            //comparando para saber qual é a temperatura maior e a menor
+            foreach (double temp in temperaturas){
+                if(temp > maior){
+                    maior = temp;
+                }
+                else if(temp < menor){
+                    menor = temp;
+                }   
+            }
+            Console.WriteLine($"A maior temperatura é {maior}");
+            Console.WriteLine($"A menor temperatura é {menor}");
+        }
+    }
+}
--- vetores/Program.cs
+++ vetores/Program.cs
@@ -6,16 +6,27 @@ namespace vetores
     {
         static void Main(string[] args)
         {
-            int[] atividade = new int[6];
-            string numeros = "", tipo = "";
-            for (int i = 1  ;  i < 6  ;  i++)
-            {
-                Console.WriteLine ($"Insira o {i} numero: ");
-                atividade[i] = int.Parse(Console.ReadLine());
-                tipo += atividade[i] % 2 == 1 ? "impar, " : "par, ";
-                numeros += atividade[i] + ", ";
+            int[] vetor = new int[6]; //para onde o número será armazenado
+            int pares =0;
+            int impares =0;
+
+            for(int cont = 0; cont <6; cont ++){ // matriz a partir do zero - menor que 6 - somando-se 1 a cada comando
+                Console.WriteLine(" Digite um número: ");
+                vetor[cont] = int.Parse(Console.ReadLine()); //ler o numero que o usuário digitou - converte string em int
+            } 
+            foreach (int num in vetor){ // para cada número inteiro dentro do meu vetor, faça isso:
+                if(num%2 == 0){
+                    pares += 1;
+                    // pares = pares +1;
+                    // pares++;
+                    }
+                else{
+                    impares += 1;                
+                    }              
             }
-            Console.Write($"Seus números são: \n{numeros}\n{tipo}");
+            Console.WriteLine($"Você tem {pares} números pares e {impares} números ímpares");
+
         }
+
     }
 }
